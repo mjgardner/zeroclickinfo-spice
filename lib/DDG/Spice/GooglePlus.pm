d2ef36a555f6bbae04732e06bee45008e01e5518
moollaza <moollaza@gmail.com>
2012-06-04 15:59:26 -0400

    modified spice from => to capture ~, but it won't work with nginx

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -4,16 +4,16 @@ package DDG::Spice::GooglePlus;
 use DDG::Spice;
 
 spice to => 'https://www.googleapis.com/plus/v1/people/$1?query=$2&key={{ENV{DDG_SPICE_GOOGLE_PLUS_APIKEY}}}&callback={{callback}}';
-spice from => '(?:([^/]*)/([^/]*)|)';
+spice from => '^([^~]*)~([^~]*)$';
 
 triggers startend => 'google+', 'google plus', 'g+', 'gplus', '+';
 
 handle remainder => sub {
 	my $query = $_;
 	if($query =~ /(\d+)$/) {
-		return $1, ''; 
+		return $1.'~'; 
 	} elsif($query =~ /((?:[a-zA-Z]|\s)+)$/) {
-		return '', $1; 
+		return '~'.$1;
 	}
 	return;
 };
